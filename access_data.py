1a5b46b03fa519ea76a4417f1b39feaeff726a2c
Olar Alex <olaralex666@gmail.com>
2023-09-11 18:50:58 +0200

    timeouts during download for large files needed to be changed...

--- access_data.py
+++ access_data.py
@@ -4,12 +4,19 @@ from bs4 import BeautifulSoup
 
 import aiofiles
 from pprint import pprint
+from aiohttp import ClientTimeout
+
+from tqdm import tqdm
 
 async def download_file_async(link, local_filename):
-    async with aiohttp.ClientSession() as session:
+    timeout = ClientTimeout(total=60 * 60)  # 1 hour
+    async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.get(link) as response:
             # Ensure the response status is OK
             response.raise_for_status()
+
+            total_size = int(response.headers.get('content-length', 0))
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=local_filename)
 
             # Asynchronously write to a local file
             async with aiofiles.open(local_filename, 'wb') as f:
@@ -18,6 +25,30 @@ async def download_file_async(link, local_filename):
                     if not chunk:
                         break
                     await f.write(chunk)
+                    progress_bar.update(len(chunk))
+
+            progress_bar.close()
+                    
+async def download_file_async(link, local_filename):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as response:
+            # Ensure the response status is OK
+            response.raise_for_status()
+            
+            total_size = int(response.headers.get('content-length', 0))
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=local_filename)
+
+            # Asynchronously write to a local file
+            async with aiofiles.open(local_filename, 'wb') as f:
+                while True:
+                    chunk = await response.content.read(8192)  # 8KB chunks
+                    if not chunk:
+                        break
+                    await f.write(chunk)
+                    progress_bar.update(len(chunk))
+
+            progress_bar.close()
+
 
 async def fetch(url, session):
     async with session.get(url) as response:
@@ -39,20 +70,16 @@ async def fetch(url, session):
             data_identifier = 'missing'
             return Exception('Missing data identifier')
         
-        pprint('Fetching: ' + url)
-        pprint('Data identifier: ' + data_identifier)
-        pprint('Download link: ' + link)
-            
         # Download and save the file
-        await download_file_async(link, './' + data_identifier)
+        await download_file_async(link, './data/' + data_identifier)
             
         return link
 
-async def main(urls, semaphore_size=10):
+async def main(urls, semaphore_size=2):
     # NOTE: limit the number of simultaneous requests using a Semaphore
     semaphore = asyncio.Semaphore(semaphore_size)
 
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(timeout=ClientTimeout(total=60 * 120)) as session:
         tasks = []
         for url in urls:
             task = asyncio.ensure_future(bounded_fetch(semaphore, url, session))
@@ -69,12 +96,15 @@ async def bounded_fetch(sem, url, session):
 
 if __name__ == "__main__":
     import json
+    from pathlib import Path
+    
+    Path('./data').mkdir(parents=True, exist_ok=True)
     
     # Load the figshare data paths
     with open('./code/figshare_data_paths.json') as f:
         data = json.load(f)
     
-    urls = [ URL['url_public_html'] for URL in data ]
+    urls = sorted([ URL['url_public_html'] for URL in data ])
     
     loop = asyncio.get_event_loop()
     results = loop.run_until_complete(main(urls))
