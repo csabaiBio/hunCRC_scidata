16cfe81429dbaea769439b929c66baf9cb6b3897
Olar Alex <olaralex666@gmail.com>
2023-09-11 19:07:34 +0200

    timeout and exceptino handling

--- access_data.py
+++ access_data.py
@@ -8,8 +8,10 @@ from aiohttp import ClientTimeout
 
 from tqdm import tqdm
 
+failed_downloads = []
+
 async def download_file_async(link, local_filename):
-    timeout = ClientTimeout(total=60 * 60)  # 1 hour
+    timeout = ClientTimeout(total=60 * 60 * 10)  # 10 hour
     async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.get(link) as response:
             # Ensure the response status is OK
@@ -28,58 +30,43 @@ async def download_file_async(link, local_filename):
                     progress_bar.update(len(chunk))
 
             progress_bar.close()
-                    
-async def download_file_async(link, local_filename):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(link) as response:
-            # Ensure the response status is OK
-            response.raise_for_status()
-            
-            total_size = int(response.headers.get('content-length', 0))
-            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=local_filename)
-
-            # Asynchronously write to a local file
-            async with aiofiles.open(local_filename, 'wb') as f:
-                while True:
-                    chunk = await response.content.read(8192)  # 8KB chunks
-                    if not chunk:
-                        break
-                    await f.write(chunk)
-                    progress_bar.update(len(chunk))
-
-            progress_bar.close()
-
 
 async def fetch(url, session):
-    async with session.get(url) as response:
-        result = await response.text()
-        
-        # Extract the download link from the HTML
-        soup = BeautifulSoup(result, 'lxml')
-        a_tag = soup.find('a', attrs={'tooltip': 'Download file'})
-        if a_tag:
-            link = a_tag['href']
-        else:
-            link = ''
+    try:
+        async with session.get(url) as response:
+            result = await response.text()
             
-        div_tag = soup.find('div', attrs={'class': 'j9gCL'})
-        
-        if div_tag:
-            data_identifier = div_tag['title']
-        else:
-            data_identifier = 'missing'
-            return Exception('Missing data identifier')
-        
-        # Download and save the file
-        await download_file_async(link, './data/' + data_identifier)
+            # Extract the download link from the HTML
+            soup = BeautifulSoup(result, 'lxml')
+            a_tag = soup.find('a', attrs={'tooltip': 'Download file'})
+            if a_tag:
+                link = a_tag['href']
+            else:
+                link = ''
+                
+            div_tag = soup.find('div', attrs={'class': 'j9gCL'})
             
-        return link
+            if div_tag:
+                data_identifier = div_tag['title']
+            else:
+                data_identifier = 'missing'
+                return Exception('Missing data identifier')
+            
+            # Download and save the file
+            await download_file_async(link, './data/' + data_identifier)
+                
+            return link
 
-async def main(urls, semaphore_size=2):
+    except Exception as e:
+        print(f"Error fetching {url} at {link}: {e}")
+        failed_downloads.append({'url_public_html': url, 'reason': str(e), 'link': link})
+        return ''
+
+async def main(urls, semaphore_size=10):
     # NOTE: limit the number of simultaneous requests using a Semaphore
     semaphore = asyncio.Semaphore(semaphore_size)
 
-    async with aiohttp.ClientSession(timeout=ClientTimeout(total=60 * 120)) as session:
+    async with aiohttp.ClientSession(timeout=ClientTimeout(total=60 * 60 * 1000)) as session:
         tasks = []
         for url in urls:
             task = asyncio.ensure_future(bounded_fetch(semaphore, url, session))
@@ -109,3 +96,6 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     results = loop.run_until_complete(main(urls))
     loop.close()
+    
+    with open('./code/failed_downloads.json', 'w') as f:
+        json.dump(failed_downloads, f)
